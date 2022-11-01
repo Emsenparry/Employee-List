c7c8cafb17f16178dcbadb0c759d3237a799b2ea
Emma Nielsen <Emmanielsen843@gmail.com>
2022-11-01 15:12:44 +0100

    comments

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,25 +1,25 @@
-let ApiData = [];
+let ApiData = []; //Empty array
 
-fetch('https://reqres.in/api/users?page=2', {
+fetch('https://reqres.in/api/users?page=2', { //Fetching the API data
     method: 'GET',
 })
 .then((response) => {
-    return response.json()
+    return response.json() //Returns response as JSON
 })
 .then((users) => {
     console.log(users);
-    ApiData = users.data
+    ApiData = users.data //Data inside users
 })
 .catch((error) => {
-    console.error(error);
+    console.error(error); //Catches error if our promise fails
 })
-.finally(() => {
+.finally(() => { //Runs finally if there are no errors
     for(let i = 0; i < ApiData.length; i++) {
         let user = ApiData[i];
         createElm(user);
     }
 })
-const createElm = (user) => {
+const createElm = (user) => { //Creates innerHTML elements with our data from the API
     document.getElementById('content').innerHTML += `
 
     <figure class="container">
@@ -29,8 +29,7 @@ const createElm = (user) => {
     <p>${user.email}</p>
     </figcaption>
    </figure>
-        `
-    
+        `   
 }
 
 
